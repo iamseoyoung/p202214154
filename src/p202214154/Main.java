9e5bf6adfc29611989c34da7a6a6fbf18ac06a39
Lee, Seoyoung <2seo02seo0@naver.com>
2025-05-20 00:34:45 +0900

    six

--- src/p202214154/Main.java
+++ src/p202214154/Main.java
@@ -9,5 +9,7 @@ System.out.println(Hello.sum(100));
 System.out.println(Hello.sum(1000)); 
 System.out.println(Sansu.max(new int[] {8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8})); 
 System.out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
+System.out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
+
 	} 
 }
